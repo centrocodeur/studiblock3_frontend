ecc4fe369a29009eb5b195014a8d48284f193c21
centrocodeur <centrocodeur@gmail.com>
2024-05-13 20:43:59 +0200

    addded _redirects file

---
+++ dist/frontend/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{b as a}from"./chunk-IZCITPWB.mjs";import"./chunk-GIA7MEPE.mjs";import"./chunk-VJBFOEQE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
